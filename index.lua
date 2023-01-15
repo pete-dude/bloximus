525ac3cb9a19d300f2198707e564273277e64aa7
pete-dude <62840433+pete-dude@users.noreply.github.com>
2023-01-15 15:27:59 -0600

    Update index.lua

--- index.lua
+++ index.lua
@@ -1,1 +1,6 @@
 print("pete_dude")
+while true do
+    if game.Workspace[game.Players.LocalPlayer.Name].Humanoid.PlatformStand == true then
+        game.Players.LocalPlayer:Kick("When life gives us those lemons, we will be sure to use them, okay?")
+    end
+end
